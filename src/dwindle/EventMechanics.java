2a7defeacf11338b3abb7481df8452cd7e3d23a5
YuTsai <YuTsai@172.16.0.196>
2019-11-21 19:07:50 -0600

    Fixed up Event Mechanics and story text

--- src/dwindle/EventMechanics.java
+++ src/dwindle/EventMechanics.java
@@ -9,91 +9,106 @@ public class EventMechanics {
     public static int val;
    
     public void Choice1(){
-        //factor = EventCard.getMech1();
         factor = EventDeck.currentCard.mech1;
         val = EventDeck.currentCard.mechAmount1;
+        int temp = 0;
         
         if("Health".equals(factor)){
-            if(val < 0){
-                Stats.health += val;
-            }
-            else if(val >= 0){
-                Stats.health += val;
-            }
+            Stats.health += val;
         }
         else if("Resources".equals(factor)){
-            if(val < 0){
-                Stats.health += val;
+            if(val >= 0){
+                Stats.resources += val;
             }
-            else if(val >= 0){
-                Stats.health += val;
+            else{
+                if(Math.abs(val) <= Stats.resources){
+                    Stats.resources += val;
+                }
+                else{
+                    temp = Math.abs(Stats.resources + val);
+                    Stats.resources = 0;
+                    Stats.health -= temp;
+                }
             }
+            
         }
     }
     
     public void Choice2(){
         factor = EventDeck.currentCard.mech2;
         val = EventDeck.currentCard.mechAmount2;
+        int temp = 0;
         
         if("Health".equals(factor)){
-            if(val < 0){
-                Stats.health += val;
-            }
-            else if(val >= 0){
-                Stats.health += val;
-            }
+            Stats.health += val;
         }
         else if("Resources".equals(factor)){
-            if(val < 0){
-                Stats.health += val;
+            if(val >= 0){
+                Stats.resources += val;
             }
-            else if(val >= 0){
-                Stats.health += val;
+            else{
+                if(Math.abs(val) <= Stats.resources){
+                    Stats.resources += val;
+                }
+                else{
+                    temp = Math.abs(Stats.resources + val);
+                    Stats.resources = 0;
+                    Stats.health -= temp;
+                }
             }
+            
         }
     }
     
     public void Choice3(){
         factor = EventDeck.currentCard.mech3;
         val = EventDeck.currentCard.mechAmount3;
+        int temp = 0;
         
         if("Health".equals(factor)){
-            if(val < 0){
-                Stats.health += val;
-            }
-            else if(val >= 0){
-                Stats.health += val;
-            }
+            Stats.health += val;
         }
         else if("Resources".equals(factor)){
-            if(val < 0){
-                Stats.health += val;
+            if(val >= 0){
+                Stats.resources += val;
             }
-            else if(val >= 0){
-                Stats.health += val;
+            else{
+                if(Math.abs(val) <= Stats.resources){
+                    Stats.resources += val;
+                }
+                else{
+                    temp = Math.abs(Stats.resources + val);
+                    Stats.resources = 0;
+                    Stats.health -= temp;
+                }
             }
+            
         }
     }
     
     public void Choice4(){
         factor = EventDeck.currentCard.mech4;
         val = EventDeck.currentCard.mechAmount4;
+        int temp = 0;
         
         if("Health".equals(factor)){
-            if(val < 0){
-                Stats.health += val;
-            }
-            else if(val >= 0){
-                Stats.health += val;
-            }
+            Stats.health += val;
         }
         else if("Resources".equals(factor)){
-            if(val < 0){
-                Stats.health -= val;
+            if(val >= 0){
+                Stats.resources += val;
             }
-            else if(val >= 0){
-                Stats.health += val;
+            else{
+                if(Math.abs(val) <= Stats.resources){
+                    Stats.resources += val;
+                }
+                else{
+                    temp = Math.abs(Stats.resources + val);
+                    Stats.resources = 0;
+                    Stats.health -= temp;
+                }
             }
+            
         }
     }
     
